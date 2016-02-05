b28e4472dc4177ada8475178268d8f4008537ff7
daniella <dbirch@boundlessgeo.com>
2016-02-05 17:11:41 -0600

    just adding some initial skeleton type things

--- entry.js
+++ entry.js
@@ -4,12 +4,21 @@ var uuid = require('uuid');
 
 //require("./style.css");
 //var content = require('./content.js');
+var id = "id";//eventually this will pass the id
 
 var Main = React.createClass({
   render: function(){
     return (
       <div>
-        Hello World
+        <p>id:id</p>//not sure how passing variables works
+        <select>//eventually these will not be static
+          <option value="gpkg">geopackage</option>
+          <option value="json">GeoJSON</option>
+        </select>
+        <select>//eventually these will not be static either
+          <option value="1.0">1.0</option>
+          <option value="2.0">2.0</option>
+        </select>
       </div>
     );
   }
