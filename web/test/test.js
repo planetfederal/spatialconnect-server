2b9fca01f3a753a36744413ad8d9bf1790d121e3
daniellabirch <dbirch@boundlessgeo.com>
2016-03-30 13:23:23 -0500

    redux test writing recipe in the works

--- web/test/test.js
+++ web/test/test.js
@@ -2,50 +2,36 @@
 //require("babel/register");
 var assert = require('assert');
 var expect = require('expect');
+import React from '../../node_modules/react';
+import TestUtils from '../../node_modules/react-addons-test-utils';
 //.DocumentRevisions-V100/import store from '../index';
- import AddEvent from '../components/AddEvent';
- import actions from '../containers/EventsContainer';
- import events from '../ducks/events';
- import NewEventForm from '../components/NewEventForm';
+import AddEvent from '../components/AddEvent';
+import actions from '../containers/EventsContainer';
+import events from '../ducks/events';
+import NewEventForm from '../components/NewEventForm';
 
-//test that the output is an unordered list and that the list items are links
-var ol='<ol>';
-var a='<a href=';
-var oloutput='<ol><li><a href="">one</a></li><li><a href="">two</a></li><li><a href="">three</a></li></ol>';
-var typeofol=typeof(ol);
-describe('#oloutput()', function () {
-  it('should return false', function () {
-    oloutput.indexOf(ol).should.equal(-1);
-  });
-});
+function setup() {
+  let props = {
+    NewEventForm: expect.createSpy()
+  }
 
-describe('#oltype()', function () {
-  it('should return true', function () {
-    assert.equal(typeofol, 'string');
-  });
-});
+  let renderer = TestUtils.createRenderer()
+  renderer.render(<Header {...props} />)
+  let output = renderer.getRenderOutput()
 
-describe('#links()', function () {
-  it('should return false', function () {
-    oloutput.indexOf(a).should.equal(-1);
-  });
-});
+  return {
+    props,
+    output,
+    renderer
+  }
+}
 
 //check that AddEvent is a string
 var typeofAddEvent=typeof(AddEvent);
 describe('AddEventType', function() {
   describe('#AddEventType()', function () {
     it('should return true', function () {
-      assert.equal(typeofAddEvent, 'function');//returns true, you need a string to evaluate the next one
-    });
-  });
-});
-
-//check that there is an onClick handler in the button
-describe('AddEvent', function() {
-  describe('#AddEvent()', function () {
-    it('should return false', function () {
-      AddEvent.indexOf('onClick').should.equal(-1);
+      assert.equal(typeofAddEvent, 'function');//returns true
     });
   });
 });
@@ -70,27 +56,14 @@ describe('#PassNewEventForm', function () {
   });
 });
 
-//Async actionCreator test (currently failing)...but where is the previous test?
-describe('#actions()', function() {
-    it('should contain actions', function() {
-      //use map to run through actions?
-      assert.equal(actions[i], events[i]);
-    });
-});
-
-//Fix double callback error
-describe('#doublecallback()', function() {
-    it('should return a callback', function() {
-      assert.equal(callbackfunction, callback);
-    });
-});
-
-describe('Array', function() {//passes
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+//test that the output is an unordered list and that the list items are links
+var ol='<ol>';
+var a='<a href=';
+var oloutput='<ol><li><a href="">one</a></li><li><a href="">two</a></li><li><a href="">three</a></li></ol>';
+var typeofol=typeof(ol);
+describe('#oloutput()', function () {
+  it('should return false', function () {
+    oloutput.indexOf(ol).should.equal(-1);
   });
 });
 
@@ -98,56 +71,5 @@ var five=5;
 describe('#five()', function () {//passes
   it('should return true', function () {
     assert.equal(five, 5);
-  });
-});
-
-//When testing synchronous code, omit the callback and Mocha will automatically continue on to the next test.
-describe('Array', function() {
-  describe('#2ndindexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-  });
-});
-
-//Testing asynchronous code with Mocha could not be simpler! Simply invoke the callback when your test is complete. By adding a callback (usually named done) to it() Mocha will know that it should wait for completion.
-//define User
-describe('User', function() {
-  describe('#save()', function() {
-    it('should save without error', function(done) {
-      var user = new User('Luna');
-      user.save(function(err) {
-        if (err) throw err;
-        done();
-      });
-    });
-  });
-});
-
-//To make things even easier, the done() callback accepts an error, so we may use this directly:
-//define User
-describe('User', function() {
-  describe('#save()', function() {
-    it('should save without error', function(done) {
-      var user = new User('Luna');
-      user.save(done);
-    });
-  });
-});
-
-//egghead tutorial
-// var helloWorld = {
-//   init() {
-//     return 'hello world';
-//   }
-// };
-// export default helloWorld;
-
-describe('helloWorld', function() {
-  describe('#init()', function() {
-    it.skip('should return hello world', function() {
-      assert.equal(helloWorld.init(), 'hello world');
-    });
   });
 });
