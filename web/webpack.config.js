4d5ac89515995e5b3bada26255ac4f3072b24906
Frank Rowe <fsrowe@gmail.com>
2016-06-27 17:16:45 -0400

    fixes for form-builder

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -5,7 +5,6 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
     path.join(__dirname, 'index')
   ],
   output: {
