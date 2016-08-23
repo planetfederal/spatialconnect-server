086a52c6ee0d2832434a909475e2753439a0cf54
Marc Cenac <mcenac@boundlessgeo.com>
2016-08-23 11:35:48 -0500

    adding TLS & JWT auth plugin to mosquitto * adding server route to check token validity

--- server/client.js
+++ server/client.js
@@ -4,53 +4,69 @@ var mqtt = require('mqtt');
 var Rx = require('rx');
 var SCMessage = require('./SCMessage');
 var _ = require('lodash');
+var AuthCommands = require('./commands/authenticate');
 
-module.exports = (host,port,clientId = 'spatialconnect_server') => {
+module.exports = (host, port, username, password, cb) => {
   if (!host || !port) {
     throw new Error('Host and Port must be set for MQTT Connection');
   } else {
     console.log('Starting Client:'+host+' on '+port);
   }
+  if (!username || !password) {
+    throw new Error('Username and password needed to authenticate to MQTT broker');
+  }
 
-  var returnObject = {};
+  AuthCommands.authenticate(username, password)
+    .subscribe(
+      (res) => {
+        if (res.token) {
+          var returnObject = {};
 
-  var client = mqtt.connect('mqtt://'+host+':'+port,{
-    clientId : clientId
-  });
+          var client = mqtt.connect('mqtt://'+host+':'+port, {
+            clientId : 'spatialconnect_server',
+            username: res.token,
+            password: 'anypass'
+          });
 
-  client.on('connect',() => {
-    console.log('Client Connected');
-  });
+          client.on('connect',() => {
+            console.log('Client Connected');
+          });
 
-  client.on('error',(err) => {
-    console.log('Client Error:'+err);
-  });
+          client.on('error',(err) => {
+            console.log('Client Error:'+err);
+          });
 
-  var messages = Rx.Observable.create((sub) => {
-    client.on('message',(t,m) => {
-      const mm = SCMessage.decode(m);
-      sub.onNext({topic:t,message:mm});
-    });
-  }).share();
+          var messages = Rx.Observable.create((sub) => {
+            client.on('message',(t,m) => {
+              const mm = SCMessage.decode(m);
+              sub.onNext({topic:t,message:mm});
+            });
+          }).share();
 
-  returnObject.listenOnTopic = (topic) => {
-    client.subscribe(topic);
-    return messages.filter((d) => d.topic === topic).map(m => m.message);
-  };
+          returnObject.listenOnTopic = (topic) => {
+            client.subscribe(topic);
+            return messages.filter((d) => d.topic === topic).map(m => m.message);
+          };
 
-  returnObject.publishObj = (topic,obj) => {
-    var newObj = _.defaults(obj,{
-      correlationId : -1,
-      replyTo : '',
-      action : -1,
-      payload : ''
-    });
-    client.publish(topic,SCMessage.encode(newObj).toBuffer());
-  };
+          returnObject.publishObj = (topic,obj) => {
+            var newObj = _.defaults(obj,{
+              correlationId : -1,
+              replyTo : '',
+              action : -1,
+              payload : ''
+            });
+            client.publish(topic,SCMessage.encode(newObj).toBuffer());
+          };
 
-  returnObject.end = () => {
-    client.end();
-  };
+          returnObject.end = () => {
+            client.end();
+          };
 
-  return returnObject;
+          cb(returnObject);
+        }
+      },
+      (err) => {
+        console.log('Could not authenticate to MQTT broker!', err);
+      }
+    );
 };
--- server/server.js
+++ server/server.js
@@ -1,20 +1,36 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-var mqttroutes = require('./mqttroutes/');
+var express    = require('express');
+var app        = express();
 var httproutes = require('./httproutes/');
-var apiroutes = require('./apiroutes/');
+var apiroutes  = require('./apiroutes/');
+var mqttClient = require('./client');
+var fs         = require('fs');
+var path       = require('path');
+var dispatcher = require('./dispatcher');
 
 var mqttBrokerHost = process.env.MQTT_BROKER_HOST || 'localhost';
 var mqttBrokerPort = process.env.MQTT_BROKER_PORT || 1883;
+var mqttUsername   = process.env.MQTT_BROKER_USERNAME;
+var mqttPassword   = process.env.MQTT_BROKER_PASSWORD;
 
 var dispatcher = require('./dispatcher');
 var TrackingCommands = require('./commands/tracking');
 
 httproutes(app);
 apiroutes(app);
-mqttroutes(mqttBrokerHost, mqttBrokerPort);
+
+mqttClient(mqttBrokerHost, mqttBrokerPort, mqttUsername, mqttPassword, (client) => {
+  var routes = {};
+  var routesPath = path.join(__dirname,'mqttroutes');
+  fs.readdirSync(routesPath)
+  .filter(file => file !== undefined && file.indexOf('.') !== 0)
+  .forEach(file => {
+    var s = require(path.join(routesPath,file))(client,dispatcher);
+    s.setupListeners();
+    routes[s.name] = s;
+  });
+});
 
 var server = app.listen(8085, function () {
   console.log('SpatialConnect-Server listening on port 8085!');
