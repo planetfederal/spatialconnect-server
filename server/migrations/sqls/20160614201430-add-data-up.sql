095ce3d7832c2293ed69dac507e21061d05d364a
Marc Cenac <mcenac@boundlessgeo.com>
2016-09-13 13:24:26 -0500

    updating inital data seed

--- server/migrations/sqls/20160614201430-add-data-up.sql
+++ server/migrations/sqls/20160614201430-add-data-up.sql
@@ -1,9 +1,7 @@
-INSERT INTO stores (name,store_type,version,uri,id) VALUES ('gj1','geojson','1','simple.geojson','3dc5afc9-393b-444c-8581-582e2c2d98a3');
+INSERT INTO stores (name,store_type,version,uri,id) VALUES ('bars','geojson','1','https://s3.amazonaws.com/test.spacon/bars.geojson','3dc5afc9-393b-444c-8581-582e2c2d98a3');
 INSERT INTO stores (name,store_type,version,uri,id) VALUES ('gj2','geojson','1','feature.geojson','3e6c072e-8e62-41be-8d1a-7a3116df9c16');
-INSERT INTO stores (name,store_type,version,uri,id) VALUES ('Haiti','gpkg','1','http://www.geopackage.org/data/haiti-vectors-split.gpkg','f6dcc750-1349-46b9-a324-0223764d46d1');
+INSERT INTO stores (name,store_type,version,uri,id) VALUES ('Rio','gpkg','1','https://s3.amazonaws.com/test.spacon/rio.gpkg','f6dcc750-1349-46b9-a324-0223764d46d1');
 INSERT INTO stores (name,store_type,version,uri,id) VALUES ('Whitehorse','gpkg','1','https://portal.opengeospatial.org/files/63156','fad33ae1-f529-4c79-affc-befc37c104ae');
-INSERT INTO devices (identifier,device_info) VALUES ('07b962d7-d091-4dad-8c4e-a083694d34b0', '{"os": "Android"}');
-INSERT INTO devices (identifier,device_info) VALUES ('0c20b743-5485-4109-823c-c9c4f54038c4', '{"os": "iOS"}');
 INSERT INTO forms (form_key,form_label) VALUES ('weed_inspector','Weed Inspector');
 
 INSERT INTO form_fields (form_id,position,field_key,field_label,type,is_integer,is_required)
@@ -53,3 +51,4 @@ VALUES ((SELECT id FROM forms WHERE form_key = 'baseball_team'),1,'why','Why?','
 INSERT INTO stores (name,store_type,version,uri,id,default_layers) VALUES ('wfstest','wfs','1.1.0','http://efc-dev.boundlessgeo.com:8080/geoserver/spatialconnect/ows','71522e9b-3ec6-48c3-8d5c-57c8d14baf6a','{"baseball_team"}');
 
 INSERT INTO users (name,email,password) VALUES ('Admin Person','admin@something.com','$2a$10$j57V/gWt9UR3wRzUC7ddye9Nhp4SWJVMm2o.lCBQe2IeE6ETztqGy');
+INSERT INTO users (name,email,password) VALUES ('Easy admin','a','$2a$10$3F76hRYXoxS4EZ8jgGY8auzLRWZb.UWbMY5b41Zrt7Nqb3c1NShlW');
