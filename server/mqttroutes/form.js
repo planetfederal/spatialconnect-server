d85153e36375fd99f98b5aa4dca2ffc403c8abba
Wes Richardet <wes@tetriscodes.com>
2016-09-13 16:35:45 -0500

    Adding MQTT Form Submission

--- server/mqttroutes/form.js
+++ server/mqttroutes/form.js
@@ -5,18 +5,15 @@ var FormCommands = require('./../commands/form');
 module.exports = (mqttClient,dispatcher) => {
   let form$ = mqttClient.listenOnTopic('/store/form');
 
-  let reply = form$.filter((d) => d.replyTo !== '');
   let message = form$.filter((d) => d.replyTo === '');
 
-  reply.subscribe(
-    (d) => {
-      mqttClient.publish(d.replyTo,'Replied To');
-    }
-  );
-
   message.subscribe(
-    (d) => {
-      console.log(d);
+    d => {
+      let data = JSON.parse(d.payload);
+      FormCommands.formSubmit(data.form_id,data.feature)
+        .subscribe(
+          dispatcher.publish(FormCommands.CHANNEL_FORM_CREATE,d.payload)
+        );
     },
     (err) => console.log(err)
   );
