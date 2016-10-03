90ab4c3bfce2fcd4c6cf97798c2d8519fad8ebb4
Frank Rowe <fsrowe@gmail.com>
2016-10-03 10:16:56 -0400

    add WS_URL config

--- web/config/development.js
+++ web/config/development.js
@@ -1,1 +1,2 @@
 export const API_URL = 'http://efc-dev.boundlessgeo.com/api/';
+export const WS_URL = 'ws://efc-dev.boundlessgeo.com:8086';
--- web/config/release.js
+++ web/config/release.js
@@ -1,1 +1,2 @@
 export const API_URL = 'http://efc-release.boundlessgeo.com/api/';
+export const WS_URL = 'ws://efc-release.boundlessgeo.com:8086';
