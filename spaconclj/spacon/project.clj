04f4089bb5847d18b527c937294e3129ec0284be
Wes Richardet <wes@tetriscodes.com>
2016-10-24 09:07:32 -0500

    Adding clj project

---
+++ spaconclj/spacon/project.clj
@@ -0,0 +1,35 @@
+(defproject spacon "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [io.pedestal/pedestal.service "0.5.1"]
+                 [io.pedestal/pedestal.jetty "0.5.1"]
+                 [ragtime "0.5.3"]
+                 [yesql "0.5.2"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc4"]
+
+                 [ch.qos.logback/logback-classic "1.1.7" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.21"]
+                 [org.slf4j/jcl-over-slf4j "1.7.21"]
+                 [org.slf4j/log4j-over-slf4j "1.7.21"]]
+
+  :def-dependencies [[lein-reload "1.0.0"]]
+
+  :plugins [[lein-environ "1.0.3"]
+            [ragtime/ragtime.lein "0.3.6"]]
+
+  :aliases {"migrate" ["run" "-m" "spacon.db/migrate"]
+            "rollback" ["run" "-m" "spacon.db/rollback"]}
+
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
+  ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.3"]]
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "spacon.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.1"]]}
+             :uberjar {:aot [spacon.server]}}
+  :main ^{:skip-aot true} spacon.server)
+
