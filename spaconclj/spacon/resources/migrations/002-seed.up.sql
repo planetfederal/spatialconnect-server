ca57c82b97cd98b266c7fe85de5a67acb77e5c58
Frank Rowe <fsrowe@gmail.com>
2016-11-21 14:42:04 -0500

    seed two teams for admin users

--- spaconclj/spacon/resources/migrations/002-seed.up.sql
+++ spaconclj/spacon/resources/migrations/002-seed.up.sql
@@ -2,3 +2,5 @@ INSERT INTO users (name,email,password) VALUES ('Admin Person','admin@something.
 INSERT INTO organizations (name) VALUES ('My Organization Name');
 INSERT INTO teams (name, organization_id) values ('My First Group Name', 1);
 INSERT INTO teams (name, organization_id) values ('Another Group Name', 1);
+INSERT INTO user_team (user_id, team_id) values (1,1);
+INSERT INTO user_team (user_id, team_id) values (1,2);
