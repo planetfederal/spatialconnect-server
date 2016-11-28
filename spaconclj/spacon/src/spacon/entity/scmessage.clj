41d6c8f4c1ecac0c4cfbb997832eee969f3a2bca
Wes Richardet <wes@tetriscodes.com>
2016-11-28 10:46:56 -0600

    Addressing Code review comments

--- spaconclj/spacon/src/spacon/entity/scmessage.clj
+++ spaconclj/spacon/src/spacon/entity/scmessage.clj
@@ -40,4 +40,3 @@
                   (or (get message :reply-to) "")
                   (or (get message :action) -1)
                   (json/write-str (or (get message :payload) "{}") :key-fn ->snake_case_string))))
-
