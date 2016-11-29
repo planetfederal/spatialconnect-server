bf3b398cd5d5c3feffe9067e468d7c112ebd6c6e
Wes Richardet <wes@tetriscodes.com>
2016-11-29 09:29:41 -0600

    Moving repl integration to user

--- spaconclj/spacon/src/spacon/server.clj
+++ spaconclj/spacon/src/spacon/server.clj
@@ -50,35 +50,6 @@
                 (new-server)
                 [:service]))))
 
-(defn init-dev []
-  (system {:http-config {:env                     :dev
-                         ::server/join?           false
-                         ::server/allowed-origins {:creds true
-                                                   :allowed-origins (constantly true)}}
-           :mqtt-config {:broker-url      (or (System/getenv "MQTT_BROKER_URL") "tcp://localhost:1883")
-                         :broker-username (or (System/getenv "MQTT_BROKER_USERNAME") "admin@something.com")}}))
-
-(def system-val nil)
-
-(defn init []
-  (alter-var-root #'system-val (constantly (init-dev))))
-
-(defn start []
-  (alter-var-root #'system-val component/start-system))
-
-(defn stop []
-  (alter-var-root #'system-val
-                  (fn [s] (when s (component/stop-system s)))))
-
-(defn go []
-  (init)
-  (start))
-
-(defn reset []
-  (stop)
-  (go))
-
-
 (defn -main
   "The entry-point for 'lein run'"
   [& args]
