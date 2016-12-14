bf05df9e8357e1968d20b5d885b834171b217611
Wes Richardet <wes@tetriscodes.com>
2016-12-14 06:55:40 -0600

    Store Cron

--- server/src/spacon/server.clj
+++ server/src/spacon/server.clj
@@ -37,10 +37,10 @@
      :mqtt (mqtt/make-mqtt-component mqtt-config)
      :ping (component/using (ping/make-ping-component) [:mqtt])
      :device (component/using (device/make-device-component) [:mqtt])
-     :store (store/make-store-component)
      :config (component/using (config/make-config-component) [:mqtt])
      :notify (component/using (notification/make-notification-component) [:mqtt])
      :trigger (component/using (trigger/make-trigger-component) [:notify])
+     :store (component/using (store/make-store-component) [:trigger])
      :location (component/using (location/make-location-component) [:mqtt :trigger])
      :form (component/using (form/make-form-component) [:mqtt :trigger])
      :service (component/using
