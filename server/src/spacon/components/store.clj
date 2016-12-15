002ae07909e2813188e830e483c4a2ad5b3dee69
Wes Richardet <wes@tetriscodes.com>
2016-12-15 09:38:45 -0600

    Mergin

--- server/src/spacon/components/store.clj
+++ server/src/spacon/components/store.clj
@@ -9,50 +9,9 @@
             [spacon.components.mqtt :as mqttapi]
             [spacon.entity.scmessage :as scm]
             [clojure.data.json :as json]
-            [overtone.at-at :refer [every,mk-pool]]
-            [clojure.xml :as xml])
+            [clojure.xml :as xml]
+            [overtone.at-at :refer [every,mk-pool,stop,stop-and-reset-pool!]])
   (:import (com.boundlessgeo.spatialconnect.schema SCCommand)))
-
-(defn http-get [context]
-  (if-let [d (storemodel/all)]
-    (response/ok d)
-    (response/error "Error")))
-
-(defn http-get-error [context]
-  (response/error "Error"))
-
-(defn http-get-store [context]
-  (if-let [d (storemodel/find-by-id (get-in context [:path-params :id]))]
-    (response/ok d)
-    (response/error "Error retrieving store")))
-
-(defn http-put-store [mqtt context]
-  (if-let [d (storemodel/update (get-in context [:path-params :id]) (:json-params context))]
-    (do (mqttapi/publish-scmessage mqtt "/config/update"
-                                   (scm/map->SCMessage
-                                    {:action (.value SCCommand/CONFIG_UPDATE_STORE)
-                                     :payload d}))
-        (response/ok "success"))
-    (response/error "Error updating")))
-
-(defn http-post-store [mqtt context]
-  (if-let [d (storemodel/create (:json-params context))]
-    (do (mqttapi/publish-scmessage mqtt "/config/update"
-                                   (scm/map->SCMessage {:action (.value SCCommand/CONFIG_ADD_STORE)
-                                                        :payload d}))
-        (response/ok d))
-    (response/error "Error creating")))
-
-(defn http-delete-store [mqtt context]
-  (let [id (get-in context [:path-params :id])]
-    (storemodel/delete (get-in context [:path-params :id]))
-    (mqttapi/publish-scmessage mqtt "/config/update"
-                               (scm/map->SCMessage {:action (.value SCCommand/CONFIG_REMOVE_STORE)
-                                                    :payload id}))
-    (response/ok "success")))
-
-(def polling-stores (ref {}))
-(def sched-pool (mk-pool))
 
 (defn feature-collection->geoms [fc]
   (if-let [features (.features fc)]
@@ -61,7 +20,7 @@
              result []]
         (if (.hasNext features)
           (recur (-> features .next .getDefaultGeometry) (conj result feature))
-          (conj result feature)))
+          result))
       [])
     []))
 
@@ -76,29 +35,77 @@
                       feature-collection->geoms)]
         (doall (map (fn [g]
                       (triggerapi/test-value trigger "STORE" g)) geoms)))
-      (println "Error "))))
+      (println "Error Fetching " url))))
 
-(defn start-polling [trigger]
-  (doall
-   (map
-    (fn [k]
-      (let [s (k @polling-stores)
-            seconds (get-in s [:options :polling])]
-        (every (* 1000 (Integer/parseInt seconds))
-               #(fetch-url trigger (:uri s)) sched-pool :initial-delay 5000)))
-    (keys @polling-stores))))
+(def polling-stores (ref {}))
+(def sched-pool (mk-pool))
 
-(defn add-polling-store [s]
+(defn start-polling [trigger store]
+  (let [seconds (get-in store [:options :polling])]
+    (every (* 1000 (Integer/parseInt seconds))
+           #(fetch-url trigger (:uri store)) sched-pool
+           :job (keyword (:id store)) :initial-delay 5000)))
+
+(defn stop-polling [store]
+  (stop (keyword (:id store))))
+
+(defn add-polling-store [trigger s]
   (if (not-empty (get-in s [:options :polling]))
-    (dosync
-     (commute polling-stores assoc (keyword (:id s)) s))))
+    (do
+      (dosync
+       (commute polling-stores assoc (keyword (:id s)) s))
+      (start-polling trigger s))))
 
 (defn remove-polling-store [id]
+  ; takes a store id string
   (dosync
-   (commute polling-stores dissoc (keyword id))))
+   (commute polling-stores dissoc (keyword id)))
+  (stop-polling (keyword id)))
 
-(defn load-polling-stores []
-  (doall (map add-polling-store (storemodel/all))))
+(defn load-polling-stores [trigger]
+  (doall (map (partial add-polling-store trigger) (storemodel/all))))
+
+(defn http-get [context]
+  (if-let [d (storemodel/all)]
+    (response/ok d)
+    (response/error "Error")))
+
+(defn http-get-error [context]
+  (response/error "Error"))
+
+(defn http-get-store [context]
+  (if-let [d (storemodel/find-by-id (get-in context [:path-params :id]))]
+    (response/ok d)
+    (response/error "Error retrieving store")))
+
+(defn http-put-store [mqtt trigger context]
+  (if-let [d (storemodel/update (get-in context [:path-params :id]) (:json-params context))]
+    (do (mqttapi/publish-scmessage mqtt "/config/update"
+                                   (scm/map->SCMessage
+                                    {:action (.value SCCommand/CONFIG_UPDATE_STORE)
+                                     :payload d}))
+        (add-polling-store trigger d)
+        (response/ok "success"))
+    (response/error "Error updating")))
+
+(defn http-post-store [mqtt trigger context]
+  (if-let [d (storemodel/create (:json-params context))]
+    (do (mqttapi/publish-scmessage mqtt "/config/update"
+                                   (scm/map->SCMessage {:action (.value SCCommand/CONFIG_ADD_STORE)
+                                                        :payload d}))
+        (add-polling-store trigger d)
+        (response/ok d))
+    (response/error "Error creating")))
+
+(defn http-delete-store [mqtt context]
+  (let [id (get-in context [:path-params :id])]
+    (storemodel/delete (get-in context [:path-params :id]))
+    (remove-polling-store id)
+    (mqttapi/publish-scmessage mqtt "/config/update"
+                               (scm/map->SCMessage {:action (.value SCCommand/CONFIG_REMOVE_STORE)
+                                                    :payload id}))
+
+    (response/ok "success")))
 
 (defn get-capabilities->layer-names [caps]
   (let [layer-names (->> caps
@@ -122,27 +129,25 @@
       (response/ok (get-capabilities->layer-names body))
       (response/error (str "Could not get capabilities from " url)))))
 
-(defn- routes [mqtt] #{["/api/stores" :get
-                        (conj intercept/common-interceptors `http-get)]
-                       ["/api/stores-error" :get
-                        (conj intercept/common-interceptors `http-get-error)]
-                       ["/api/stores/:id" :get
-                        (conj intercept/common-interceptors `http-get-store)]
-                       ["/api/stores/:id" :put
-                        (conj intercept/common-interceptors (partial http-put-store mqtt)) :route-name :http-put-store]
-                       ["/api/stores" :post
-                        (conj intercept/common-interceptors (partial http-post-store mqtt)) :route-name :http-post-store]
-                       ["/api/stores/:id" :delete
-                        (conj intercept/common-interceptors (partial http-delete-store mqtt)) :route-name :http-delete-store]
-                       ["/api/wfs/getCapabilities" :get
-                        (conj intercept/common-interceptors `http-get-capabilities)]})
+(defn- routes [mqtt trigger]
+  #{["/api/stores" :get
+                                (conj intercept/common-interceptors `http-get)]
+                               ["/api/stores-error" :get
+                                (conj intercept/common-interceptors `http-get-error)]
+                               ["/api/stores/:id" :get
+                                (conj intercept/common-interceptors `http-get-store)]
+                               ["/api/stores/:id" :put
+                                (conj intercept/common-interceptors (partial http-put-store mqtt trigger)) :route-name :http-put-store]
+                               ["/api/stores" :post
+                                (conj intercept/common-interceptors (partial http-post-store mqtt trigger)) :route-name :http-post-store]
+                               ["/api/stores/:id" :delete
+                                (conj intercept/common-interceptors (partial http-delete-store mqtt)) :route-name :http-delete-store]})
 
 (defrecord StoreComponent [mqtt trigger]
   component/Lifecycle
   (start [this]
-    (load-polling-stores)
-    (start-polling trigger)
-    (assoc this :routes (routes mqtt)))
+    (load-polling-stores trigger)
+    (assoc this :routes (routes mqtt trigger)))
   (stop [this]
     this))
 
