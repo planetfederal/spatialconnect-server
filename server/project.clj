5f3a80e1dea16b034fc0bad7a4ac097babfd043d
Wes Richardet <wes@tetriscodes.com>
2017-01-17 09:36:12 -0600

    Bumping version to 0.8.0

--- server/project.clj
+++ server/project.clj
@@ -1,6 +1,6 @@
-(defproject spacon "0.7.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject spacon "0.8.0-SNAPSHOT"
+  :description "SpatialConnect Server"
+  :url "http://github.com/boundlessgeo/spatialconnect-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
