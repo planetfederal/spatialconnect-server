aa6354fb209501e762e3b8b444429445c1c8aa20
Marc Cenac <mcenac@boundlessgeo.com>
2017-01-18 12:07:08 -0600

    adding tls with letsencrypt certs

--- web/config/development.js
+++ web/config/development.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://efc-dev.boundlessgeo.com/api/';
-export const WS_URL = 'ws://efc-dev.boundlessgeo.com:8086';
+export const API_URL = 'https://dev.signal.boundlessgeo.com/api/';
+export const WS_URL = 'ws://dev.signal.boundlessgeo.com:8086';
