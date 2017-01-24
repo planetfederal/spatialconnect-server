368098e965ff9448baaafa414b8b4744a433941e
Marc Cenac <mcenac@boundlessgeo.com>
2017-01-24 15:54:34 -0600

    adding tests for store component

--- server/test/spacon/store_test.clj
+++ server/test/spacon/store_test.clj
@@ -5,28 +5,105 @@
             [clojure.spec :as spec]
             [clojure.spec.gen :as gen]
             [camel-snake-kebab.core :refer :all]
-            [camel-snake-kebab.extras :refer [transform-keys]]))
+            [camel-snake-kebab.extras :refer [transform-keys]]
+            [spacon.components.mqtt.core :as mqttapi]
+            [clojure.data.json :as json]
+            [spacon.entity.scmessage :as scm])
+  (:import (java.net URLEncoder URLDecoder)
+           (com.boundlessgeo.spatialconnect.schema SCCommand)))
+
+(use-fixtures :once utils/setup-fixtures)
 
 (deftest store-list-test
   (is (true? (utils/spec-passed? `store/all))))
 
-(use-fixtures :once utils/setup-fixtures)
+(defn generate-test-store []
+  (->> (gen/generate (spec/gen :spacon.specs.store/store-spec))
+       (transform-keys ->snake_case_keyword)))
 
-(deftest store-get-test []
-  (let [res (utils/request-get "/api/stores")]
-    (is (some? (:result res)))))
-
-(deftest store-crud-test []
+(deftest store-http-crud-test
   (let [token (utils/authenticate "admin@something.com" "admin")
         auth {"Authorization" (str "Token " token)}
-        store (gen/generate (spec/gen :spacon.specs.store/store-spec))
-        t_e_a_m (transform-keys ->snake_case_keyword store)
-        new-store (utils/request-post "/api/stores" t_e_a_m auth)
-        create-res (transform-keys ->kebab-case-keyword (:result new-store))
-        read-store (utils/request-get (str "/api/stores/" (:id create-res)) auth)
-        read-res (transform-keys ->kebab-case-keyword (:result read-store))
-        delete-store (utils/request-delete (str "/api/stores/" (:id create-res)) auth)
-        delete-res (transform-keys ->kebab-case-keyword (:result delete-store))]
-    (is (spec/valid? :spacon.specs.store/store-spec create-res))
-    (is (spec/valid? :spacon.specs.store/store-spec read-res))
-    (is (= "success" delete-res))))
+        test-store (generate-test-store)]
+
+    (testing "Creating a store through REST api produces a valid HTML response"
+      (let [res (utils/request-post "/api/stores" test-store auth)
+            new-store (transform-keys ->kebab-case-keyword (:result res))]
+        (is (contains? res :result)
+            "Response should have result keyword")
+        (is (spec/valid? :spacon.specs.store/store-spec new-store)
+            "The response should contain a store that conforms to the store spec")))
+
+    (testing "Retrieving all stores through REST api produces a valid HTML response"
+      (let [res (-> (utils/request-get "/api/stores" auth))
+            store (->> res :result first (transform-keys ->kebab-case-keyword))]
+        (is (contains? res :result)
+            "Response should have result keyword")
+        (is (spec/valid? :spacon.specs.store/store-spec store)
+            "The response should contain a store that conforms to the store spec")))
+
+    (testing "Retrieving store by its key through REST api produces a valid HTML response"
+      (let [s (-> (utils/request-get "/api/stores" auth) :result first)
+            res (-> (utils/request-get (str "/api/stores/" (:id s)) auth))
+            store (transform-keys ->kebab-case-keyword (:result res))]
+        (is (contains? res :result)
+            "Response should have result keyword")
+        (is (spec/valid? :spacon.specs.store/store-spec store)
+            "The response should contain a store that conforms to the store spec")))
+
+    (testing "Updating a store through REST api produces a valid HTML response"
+      (let [store (-> (utils/request-get "/api/stores" auth) :result first)
+            renamed-store (->> (assoc store :name "foo") (transform-keys ->snake_case_keyword))
+            res (utils/request-put (str "/api/stores/" (:id store)) renamed-store auth)
+            updated-store (transform-keys ->kebab-case-keyword (:result res))]
+        (is (contains? res :result)
+            "Response should have result keyword")
+        (is (spec/valid? :spacon.specs.store/store-spec updated-store)
+            "The response should contain a store that conforms to the store spec")
+        (is (= "foo" (:name updated-store))
+            "The response should contain the updated store label")))
+
+    (testing "Deleting stores through REST api produces a valid HTML response"
+      (let [store (-> (utils/request-get "/api/stores" auth) :result first)
+            res (utils/request-delete (str "/api/stores/" (:id store)) auth)]
+        (is (= "success" (:result res))
+            "The response should contain a success message")))
+
+    (testing "Creating a store through REST api produces a valid message on config/update topic"
+      (let [mqtt (:mqtt user/system-val)
+            msg-handler (fn [name m]
+                          (is (= name (get-in m [:payload :name])))
+                          (is (= (.value SCCommand/CONFIG_ADD_STORE) (:action m))))]
+        (mqttapi/subscribe mqtt "/config/update" (partial msg-handler (:name test-store)))
+        (utils/request-post "/api/stores" test-store auth)
+        (Thread/sleep 1000)))
+
+    (testing "Updating a store through REST api produces a valid message on config/update topic"
+      (let [mqtt (:mqtt user/system-val)
+            store (-> (utils/request-get "/api/stores" auth) :result first)
+            updated-store (->> (assoc store :name "foo") (transform-keys ->snake_case_keyword))
+            msg-handler (fn [id m]
+                          (is (= id (get-in m [:payload :id])))
+                          (is (= (.value SCCommand/CONFIG_UPDATE_STORE) (:action m))))]
+        (mqttapi/subscribe mqtt "/config/update" (partial msg-handler (:id store)))
+        (utils/request-put (str "/api/stores/" (:id store)) updated-store auth)
+        (Thread/sleep 1000)))
+
+    (testing "Deleting a store through REST api produces a valid message on config/update topic"
+      (let [mqtt (:mqtt user/system-val)
+            store (-> (utils/request-get "/api/stores" auth) :result first)
+            msg-handler (fn [id m]
+                          (is (= id (get-in m [:payload :id])))
+                          (is (= (.value SCCommand/CONFIG_REMOVE_STORE) (:action m))))]
+        (mqttapi/subscribe mqtt "/config/update" (partial msg-handler (:id store)))
+        (utils/request-delete (str "/api/stores/" (:id store)) auth)
+        (Thread/sleep 1000)))))
+
+(deftest wfs-get-caps-proxy
+  (testing "Sending a request to /api/wfs/getCapabilities?url=<wfs-endpoint> returns a list of layernames"
+    (let [token (utils/authenticate "admin@something.com" "admin")
+          auth {"Authorization" (str "Token " token)}
+          wfs-url (URLEncoder/encode "http://demo.boundlessgeo.com/geoserver/osm/ows" "UTF-8")
+          res (utils/request-get (str "/api/wfs/getCapabilities?url=" wfs-url) auth)]
+      (is (< 0 (count (:result res)))
+          "The response should contain a list of layers in the result"))))
