c6534e1b436474415ccb9dd48e2024ff3c1adc9e
Marc Cenac <mcenac@boundlessgeo.com>
2017-02-06 16:48:58 -0600

    create an http endpoint to generate sample form submission features

--- server/src/spacon/http/response.clj
+++ server/src/spacon/http/response.clj
@@ -6,26 +6,32 @@
 (defn is-error? [status]
   (< 300 status))
 
-(defn response
-  "Creates a response map using the status, body, and headers.
-  Also transforms response body keys to be snake_case"
+(defn make-response
+  "Creates a response map using the status, body, and headers."
   [status body & {:as headers}]
-  (let [b_o_d_y (transform-keys ->snake_case_keyword body)
-        res-body (assoc {}
-                        :result (if (is-error? status) nil b_o_d_y)
-                        :error (if (is-error? status) b_o_d_y nil))]
+  (let [res-body (assoc {}
+                   :result (if (is-error? status) nil body)
+                   :error (if (is-error? status) body nil))]
     (let [res {:status status :body res-body :headers headers}]
       (log/trace "Returning response" res)
       res)))
 
+(defn make-default-response
+  "Creates a response map using the status, body, and headers.
+  Also transforms response body keys to be snake_case"
+  [status body & {:as headers}]
+  (let [b_o_d_y (transform-keys ->snake_case_keyword body)]
+      (make-response status b_o_d_y)))
 
-(def ok (partial response 200))
-(def created (partial response 201))
-(def accepted (partial response 202))
-(def bad-request (partial response 400))
-(def unauthorized (partial response 401))
-(def forbidden (partial response 403))
-(def not-found (partial response 404))
-(def conflict (partial response 409))
-(def error (partial response 500))
-(def unavailable (partial response 503))
+
+(def ok-without-snake-case (partial make-response 200))
+(def ok (partial make-default-response 200))
+(def created (partial make-default-response 201))
+(def accepted (partial make-default-response 202))
+(def bad-request (partial make-default-response 400))
+(def unauthorized (partial make-default-response 401))
+(def forbidden (partial make-default-response 403))
+(def not-found (partial make-default-response 404))
+(def conflict (partial make-default-response 409))
+(def error (partial make-default-response 500))
+(def unavailable (partial make-default-response 503))
