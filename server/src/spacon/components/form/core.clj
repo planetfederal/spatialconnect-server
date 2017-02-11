a7f2ab61f9d4d4af184ef34dee6f379d7e88560e
Marc J Cenac <mrcnc@users.noreply.github.com>
2017-02-11 17:59:57 -0600

    set the default version of a form to 1 if it's not in the request body (#252)

--- server/src/spacon/components/form/core.clj
+++ server/src/spacon/components/form/core.clj
@@ -50,7 +50,10 @@
   "Creates a new form using the json body then publishes a
   config update message about the newly added form"
   [mqtt request]
-  (let [form (:json-params request)]
+  (let [f (:json-params request)
+        form (if (not (contains? f :version))
+               (assoc f :version 1)
+               f)]
     (log/debug "Validating form")
     (if (s/valid? :spacon.specs.form/form-spec form)
       (let [new-form (formmodel/add-form-with-fields form)]
