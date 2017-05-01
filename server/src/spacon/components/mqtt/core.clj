00f281c1939b48ba6cdcdbb071b1d82179d225c9
georific <lrobinson@boundlessgeo.com>
2017-05-01 13:58:22 -0400

    Adding PR #283 changes

--- server/src/spacon/components/mqtt/core.clj
+++ server/src/spacon/components/mqtt/core.clj
@@ -21,9 +21,13 @@
             [clojure.core.async :as async]
             [spacon.components.http.auth :refer [get-token]]
             [clojure.tools.logging :as log]
-            [spacon.components.queue.protocol :as queue]))
+            [spacon.components.queue.protocol :as queue])
+  (:import (org.eclipse.paho.client.mqttv3 MqttException)
+           (java.net InetAddress)))
 
-(def client-id "spacon-server")
+(def client-id (or (System/getenv "MQTT_CLIENT_ID")
+                   (subs (str "sc-" (InetAddress/getLocalHost)) 0 22 )))
+(defonce conn (atom nil))
 (def action-topic {:register-device "/config/register"
                      :full-config "/config"
                      :config-update "/config"
@@ -53,35 +57,51 @@
   "Connect to mqtt broker at url
    Example url: ssl://broker.hostname.domain:port"
   [url]
-  (log/debug "Connecting MQTT Client at" url)
-  (mh/connect url client-id))
+  (while (or (nil? @conn) (not (mh/connected? @conn)))
+    (log/debugf "Connecting MQTT Client to %s" url)
+    (try
+      (do
+        (reset! conn (mh/connect url client-id))
+        (log/infof "MQTT Client connected to %s" url))
+      (catch MqttException e
+        (do
+          (log/error e "Failed to connect to" url)
+          ; wait 4 seconds befor trying again
+          (Thread/sleep 4000))))))
 
 ; publishes message on the send channel
 (defn- publish [mqtt topic message]
-  (log/tracef "Publishing to topic %s %nmessage: %s" topic message)
+  (log/debugf "Publishing to topic %s %nmessage: %s" topic message)
   (async/go (async/>!! (:publish-channel mqtt) {:topic topic :message (cm/message->bytes message)})))
 
 ; receive message on subscribe channel
 (defn- receive [mqtt topic message]
-  (log/tracef "Received message on topic %s %nmessage:%s" topic (cm/from-bytes message))
+  (log/tracef "Received message on topic: %nmessage: %s" topic (cm/from-bytes message))
   (if (nil? message)
     (log/debug "Nil message on topic " topic)
     (async/go (async/>!! (:subscribe-channel mqtt) {:topic topic :message (cm/from-bytes message)}))))
+
+(defn reconnect [reason]
+  (let [url (or (System/getenv "MQTT_BROKER_URL") "tcp://localhost:1883")]
+    (log/debugf "%s%nConnection lost. Attempting reconnect to %s" reason url)
+    (connectmqtt url)))
 
 (defn subscribe
   "Subscribe to mqtt topic with message handler function f"
   [mqtt topic f]
   (log/debug "Subscribing to topic" topic)
   (add-topic topic f)
-  (mh/subscribe (:conn mqtt) {topic 2} (fn [^String topic _ ^bytes payload]
-                                         (receive mqtt topic payload))))
+  (mh/subscribe @conn {topic 2}
+                (fn [^String topic _ ^bytes payload]
+                  (receive mqtt topic payload))
+                {:on-connection-lost reconnect}))
 
 (defn unsubscribe
   "Unsubscribe to mqtt topic"
   [mqtt topic]
   (log/debug "Unsubscribing to topic" topic)
   (remove-topic topic)
-  (mh/unsubscribe (:conn mqtt) topic))
+  (mh/unsubscribe @conn topic))
 
 (defn- process-publish-channel [mqtt chan]
   (async/go (while true
@@ -89,8 +109,13 @@
                     t (:topic v)
                     m (:message v)]
                 (try
-                  (if-not (or (nil? t) (nil? m))
-                    (mh/publish (:conn mqtt) t m))
+                  (if-not (mh/connected? @conn)
+                    (do
+                      (reconnect nil)
+                      (if-not (or (nil? t) (nil? m))
+                        (mh/publish @conn t m)))
+                    (if-not (or (nil? t) (nil? m))
+                      (mh/publish @conn t m)))
                   (catch Exception e
                     (log/error "Could not publish message b/c"
                                (.getLocalizedMessage e))))))))
@@ -129,7 +154,7 @@
     (log/debug "Stopping MQTT Component")
     (async/close! (:publish-channel this))
     (async/close! (:subscribe-channel this))
-    (mh/disconnect (:conn this))
+    (mh/disconnect @conn)
     this)
   (publish [this connectMessage]
     (publish this (:to connectMessage) connectMessage))
