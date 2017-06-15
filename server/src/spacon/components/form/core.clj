85bb513395d7ebc57b12b0dd4368abd78b20448f
Frank Rowe <fsrowe@gmail.com>
2017-06-15 09:27:32 -0400

    geometry is required, but can be nil

--- server/src/spacon/components/form/core.clj
+++ server/src/spacon/components/form/core.clj
@@ -110,8 +110,7 @@
                json/write-str
                jtsio/read-feature
                .getDefaultGeometry)]
-    (let [valid-feature (if (or (s/valid? :spacon.specs.geojson/pointfeature-spec form-data)
-                                (s/valid? :spacon.specs.geojson/nogeom-spec form-data))
+    (let [valid-feature (if (s/valid? :spacon.specs.geojson/pointfeature-spec form-data)
                           (do
                             (log/debug "Submitting form data")
                             (formmodel/add-form-data form-data form-id device-identifier)
